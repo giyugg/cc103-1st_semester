dcdf95486a7d3c1a4fce2eeb7eb55ce793ac0a6f
GiyuuuGG <personal.kyleacuna@gmail.com>
2025-09-23 15:31:37 +0800

    Finished my activity for EmployeeSalaryCalculator hehe

--- EmployeeSalaryCalculator.java
+++ EmployeeSalaryCalculator.java
@@ -1,5 +1,79 @@
+import java.util.Scanner;
 public class EmployeeSalaryCalculator {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Variable declarations: Employee details
+        String employeeName;
+        String departmentCode;
+        String statusCode;
+        // Variable declarations: Hour time based details
+        int hoursWorked;
+        int ratePerHour;
+        // Variable declarations: Salary details
+        double grossIncome;
+        // Variable declarations: Deductions
+        double birTax;
+        double securityTax;
+        double totalDeductions;
+        // Variable declarations: Net Income
+        double netIncome;
+
+        // Input: Employee details
+        System.out.print("Enter Employee Name: ");
+        employeeName = input.nextLine();
+
+        System.out.print("Enter Department Code: ");
+        departmentCode = input.nextLine();
+        if (departmentCode.equals("MIS")) { // Management Information Systems
+            ratePerHour = 500;
+        }
+        else if (departmentCode.equals("MKT")) { // Marketing
+            ratePerHour = 400;
+        }
+        else if (departmentCode.equals("ACT")) { // Accounting
+            ratePerHour = 550;
+        }
+        else {
+            System.out.println("Invalid Department Code");
+            System.out.println("Goodbye...");
+            input.close();
+            return;
+        }
         
+        System.out.print("Enter Hours Worked: ");
+        hoursWorked = input.nextInt();
+        input.nextLine(); // fix bug that skips next input
+
+        System.out.print("Enter Status Code: ");
+        statusCode = input.nextLine();
+        if (statusCode.equals("HOF")) { // Head of Family
+            birTax = 0.1;
+            securityTax = 0.05;
+        }
+        else if (statusCode.equals("SWD")) { // Single with Dependents
+            birTax = 0.15;
+            securityTax = 0.1;
+        }
+        else if (statusCode.equals("SOD")) { // Single without Dependents
+            birTax = 0.2;
+            securityTax = 0.15;
+        }
+        else {
+            System.out.println("Invalid Status Code");
+            System.out.println("Goodbye...");
+            input.close();
+            return;
+        }
+
+        // Process: Salary calculations
+        grossIncome = hoursWorked * ratePerHour;
+        System.out.println("\nGross Salary is: PHP " + (grossIncome)); // added newline para maganda
+        totalDeductions = (birTax + securityTax) * grossIncome;
+        System.out.println("Total Deductions is: PHP " + (totalDeductions));
+        netIncome = grossIncome - totalDeductions;
+        System.out.println("Net Salary is: PHP " + (netIncome));
+
+        input.close();
     }
 }
